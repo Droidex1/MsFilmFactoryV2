fa5bdd4d98411a8f6b552116fa75eff0e5370a53
Prabhu K <105587688+Droidex1@users.noreply.github.com>
2023-02-04 22:45:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -20,6 +20,10 @@ from database.filters_mdb import (
     find_filter,
     get_filters,
 )
+
+import os
+req_channel = int(os.environ.get('REQ_CHANNEL', '-1001865437525'))
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -677,6 +681,7 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
+                await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝**Content Name :**`{search}`\n**Requested By:** `{message.from_user.first_name}`\n **USER ID:**`{message.from_user.id}`\n\n🗃",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Mark as Done 🔺", callback_data="close_data")]]))
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
