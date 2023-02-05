5cc5868c9bbd08ac3f6aa35ca37b967ee5fd41cf
Prabhu K <105587688+Droidex1@users.noreply.github.com>
2023-02-05 16:32:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/d088df0b31174957d25b1.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '857412825 5103557143').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001885153855 -1001456266233').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001283820046')
@@ -85,7 +85,7 @@ DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
 DOWNLOAD_TEXT_URL = "https://t.me/ChostEngine/248"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Get Updates"
+CAPTION_BUTTON = "New Uploads"
 CAPTION_BUTTON_URL = "https://t.me/+Ja7SvobwfQpiN2I1"
 
    # Auto Delete For Bot Sending Files #
