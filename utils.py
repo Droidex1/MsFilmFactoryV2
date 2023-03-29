2608d9fba47fd554a66a9170c2c6f0af06b68011
Prabhu K <105587688+Droidex1@users.noreply.github.com>
2023-03-29 19:02:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://mdisklink.link/api'
+    url = f'https://tinyfy.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,        
               }
